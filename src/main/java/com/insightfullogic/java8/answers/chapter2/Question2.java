fec8d5aec6506b615c8a466999e6419edcb1ad5e
txidol <love_xiang21@foxmail.com>
2016-07-15 11:30:09 +0800

    print

--- src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
+++ src/main/java/com/insightfullogic/java8/answers/chapter2/Question2.java
@@ -1,12 +1,16 @@
 package com.insightfullogic.java8.answers.chapter2;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import javax.swing.text.DateFormatter;
+import java.util.Date;
 
-import static java.lang.ThreadLocal.withInitial;
+import javax.swing.text.DateFormatter;
 
 public class Question2 {
 
      public final static ThreadLocal<DateFormatter> formatter = ThreadLocal.withInitial(() -> new DateFormatter(new SimpleDateFormat("dd-MMM-yyyy")));
 
+     public static void main(String[] args) throws ParseException {
+    	 System.out.println(formatter.get().valueToString(new Date()));
+	}
 }
